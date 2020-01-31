3a79c6550b8d298ab0687ac661c8fbb81d62d82c
gentashibasaki <genta.shibasaki.78@gmail.com>
2020-01-31 17:51:38 +0900

    working on redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "../styles/styles.css";
 import Navbar from "./Navbar";
 import AllPhotos from "./AllPhotos";
 import SinglePhoto from "./SinglePhoto";
 import { listObjects, getSingleObject, saveObject } from "../utils/index";
 import { useSelector, useDispatch } from "react-redux";
-import { changeView, addAllPhotos } from "./redux";
+import { changeView, setAllPhotos, setOnePhoto, store } from "./redux";
 
 export default function App() {
   const view = useSelector(state => state.currentView);
-  const photos = useSelector(state => state.photos);
-  const singlePhoto = useSelector(state => state.selectedPhoto);
   const dispatch = useDispatch();
   const [photoView, setPhotoView] = useState("");
   const onButtonClick = () => {
-    dispatch(changeView(photoView));
+    dispatch(changeView(true));
   };
 
+  const photos = useSelector(state => state.photos);
+  const getPhotos = async () => {
+    const photoObjects = await listObjects(90);
+    const photoBase64Strings = await Promise.all(
+      photoObjects.map(async photoObject => {
+        let tmp = await getSingleObject(photoObject.Key);
+        return `data:image/png;base64, ${tmp}`;
+      })
+    );
+    dispatch(setAllPhotos(photoBase64Strings));
+  };
+  useEffect(() => {
+    getPhotos();
+  });
+
+  const singlePhoto = useSelector(state => state.selectedPhoto);
+  const selectOnePhoto = photo => {
+    dispatch(changeView(false));
+    dispatch(setOnePhoto(photo));
+  };
   //WIP
 
   return (
     <div className="App">
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
       <section id="photos">
-        {view === "AllPhotos" ? (
-          photos.map(photo => <AllPhotos />)
+        {view ? (
+          photos.map(photo => <AllPhotos selectOnePhoto={selectOnePhoto} />)
         ) : (
-          <SinglePhoto />
+          <SinglePhoto onButtonClick={onButtonClick} />
         )}
       </section>
     </div>
--- src/components/redux.js
+++ src/components/redux.js
@@ -1,31 +1,34 @@
 import { createStore } from "redux";
 //WIP
 const initialState = {
-  currentView: "AllPhotos",
+  currentView: true,
   photos: [],
   selectedPhoto: ""
 };
 
-export const changeView = text => ({
-  type: "ChangeCurrentView",
-  text
+export const changeView = input => ({
+  type: "SET_CURRENT_VIEW",
+  input
 });
 
-export const addAllPhotos = photosData => ({
-  type: "addAllPhotos",
+export const setAllPhotos = photosData => ({
+  type: "SET_ALL_PHOTOS",
   photosData
+});
+
+export const setOnePhoto = photoData => ({
+  type: "SET_ONE_PHOTO",
+  photoData
 });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ChangeCurrentView":
-      console.log(state);
-      state.currentView =
-        action.text === "AllPhotos" ? "SinglePhoto" : "AllPhotos";
-      return state;
-    case "addAllPhotos":
-      state.photos = action.photosData;
-      return state;
+    case "SET_CURRENT_VIEW":
+      return { ...state, currentView: action.input };
+    case "SET_ALL_PHOTOS":
+      return { ...state, photos: action.photosData };
+    case "SET_ONE_PHOTO":
+      return { ...state, selectedPhoto: action.photoData };
     default:
       return state;
   }
